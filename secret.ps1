0a6fd9df2389b20e9dc66da5f53135df6c907074
Damir Huric <87942505+usill@users.noreply.github.com>
2025-03-26 13:27:44 +0300

    gitignore

--- secret.ps1
+++
@@ -1,5 +0,0 @@
-﻿dotnet user-secrets init
-dotnet user-secrets set "Auth:Jwt:Issuer" "DamirHuric"
-dotnet user-secrets set "Auth:Jwt:Audience" "Messanger"
-dotnet user-secrets set "Auth:Jwt:Key" "OnN8EAhdfK_YAAlnJUtFELK_hlzMBAnDrXyw4_SsnRgjQXurbh_1d0kMz8jF_zC0PGWNX"
-dotnet user-secrets set "PasswordSalt" "BVvcHf1746Jk2HM9qmHWLPVL4iDEBaERlVHYYZke8IcFkFeF8sbLFpPGT4Gpkq9C"
